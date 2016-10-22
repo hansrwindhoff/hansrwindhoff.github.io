0526a0aa88f015fb870b2cc1c2da541d1f575454
hansrwindhoff <hansrwindhoff@sovrn.com>
2016-10-21 20:39:30 -0600

    ed

--- RTPVehicles/service-worker-registration.js
+++ RTPVehicles/service-worker-registration.js
@@ -17,7 +17,7 @@
 /* eslint-env browser */
 'use strict';
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
   }).catch(function(err) {
